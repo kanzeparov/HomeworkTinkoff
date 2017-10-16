efcb32c36e44bd30a3ec509931e10f0183f9c261
kanzeparov <kanzeparov@yandex.ru>
2017-10-16 21:17:28 +0300

    project

---
+++ app/src/main/java/com/example/franck/fintechhomework/fragments/MyFragmentListener.java
@@ -0,0 +1,8 @@
+package com.example.franck.fintechhomework.fragments;
+
+/**
+ * Created by Franck on 16.10.2017.
+ */
+
+public interface MyFragmentListener {
+}
---
+++ app/src/main/java/com/example/franck/fintechhomework/fragments/ResultFragment.java
@@ -0,0 +1,10 @@
+package com.example.franck.fintechhomework.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Franck on 16.10.2017.
+ */
+
+public class ResultFragment extends Fragment {
+}
