1dc9f1da50f7afabb2d4531ffc550dcbf99af9b3
kanzeparov <kanzeparov@yandex.ru>
2017-10-31 16:46:01 +0300

    part1

---
+++ app/src/main/java/com/example/franck/fintechhomework/Dot.java
@@ -0,0 +1,15 @@
+package com.example.franck.fintechhomework;
+
+/**
+ * Created by Franck on 30.10.2017.
+ */
+
+public class Dot {
+    public int x;
+    public int y;
+
+    public Dot(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
--- app/src/main/java/com/example/franck/fintechhomework/MainActivity.java
+++ app/src/main/java/com/example/franck/fintechhomework/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.franck.fintechhomework;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LineChartView lineChartView = (LineChartView) findViewById(R.id.linechart);
+        lineChartView.setChartData(getRandomData());
+    }
+
+
+    private float[] getRandomData() {
+        return new float[] { 10, 12, 7, 14, 15, 19, 13, 2, 10, 13, 13, 10, 15, 14 };
     }
 }
