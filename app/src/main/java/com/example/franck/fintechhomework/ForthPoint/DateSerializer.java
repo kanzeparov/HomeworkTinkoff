aab552fda308fdd0ccd0c5e5ccbb1a350cf7066b
kanzeparov <kanzeparov@yandex.ru>
2017-11-07 16:18:28 +0300

    done

---
+++ app/src/main/java/com/example/franck/fintechhomework/ForthPoint/DateSerializer.java
@@ -0,0 +1,8 @@
+package com.example.franck.fintechhomework.ForthPoint;
+
+/**
+ * Created by Franck on 07.11.2017.
+ */
+
+public class DateSerializer {
+}
---
+++ app/src/main/java/com/example/franck/fintechhomework/ThirdPoint/MoneyAmountDeserializer.java
@@ -0,0 +1,8 @@
+package com.example.franck.fintechhomework.ThirdPoint;
+
+/**
+ * Created by Franck on 07.11.2017.
+ */
+
+public class MoneyAmountDeserializer {
+}
